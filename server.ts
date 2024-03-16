0aa15a97c3b7a62ce6784d22647756020805658d
Bruno Blauzius Schuindt <bruno.blauzius@lojacorr.local>
2024-03-16 16:59:57 -0300

    config version

--- server.ts
+++ server.ts
@@ -34,7 +34,7 @@ export default class Server{
         this.app.use(Logger.log);
 
         this.app.route('/').get((req, res) => {
-            res.json({'version' : '1.0.0'});
+            res.json({'version' : '1.1.1'});
         });
         this.app.use('/api/v1/', userRouter(this.dbconnection));
         this.app.use(Auth.validate);
